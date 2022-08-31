fc12c3414bf2a0a5e61a8d9cccb049484361d5fc
Ilsur <yuzlikeyevi@gmail.com>
2022-08-30 22:05:40 -0500

    Created database drinks and linked to main server file

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require("express");
+const drinks = require("./models/drinks")
 const app = express();
 const port = 3000;
+
+// listen to port and console log to check if its running
+app.listen(port, () => {
+    console.log("I can hear server running")
+});
 
 app.get("/", (req, res) => {
     res.send("Welcome to the Gitpub App!")
 })
 
-// listen to port and console log to check if its running
-app.listen(port, () => {
-    console.log("I can hear server running")
+app.get("/drinks/", (req, res) => {
+    res.send(drinks)
 })
 
